63be39f00bc0ba2688b2857842fc09d8715d14e2
Jordan <j.betts11599@gmail.com>
2022-03-15 17:54:37 +0000

    SQL profile reading

--- profiles.js
+++ profiles.js
@@ -1,1 +1,28 @@
 const Discord = require("discord.js");
+const sqlite3 = require("sqlite3").verbose();
+
+function read(path, targetChannel) {
+  db = new sqlite3.Database(path, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    const sqlRead = "SELECT * FROM players";
+
+    db.all(sqlRead, [], (err, rows) => {
+      if (err) return console.error(err.message);
+
+      rows.forEach((row) => {
+        console.log(row);
+      });
+
+      db.close((err) => {
+        if (err) return console.error(err.message);
+      });
+      console.log("db closed");
+    });
+
+    targetChannel.send("Connected to the Item database.");
+  });
+}
+
+module.exports.read = read;
