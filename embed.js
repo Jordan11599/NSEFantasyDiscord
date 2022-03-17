9a7e88b318867bcf6b284bbd3804c0f1af9f49b4
Jordan <j.betts11599@gmail.com>
2022-03-17 17:56:51 +0000

    added new template

--- embed.js
+++ embed.js
@@ -9,3 +9,28 @@ exports.errorMessage = new Discord.MessageEmbed()
   .setColor("#FF0000")
   .setTitle("⛔ Error: Unknown Command")
   .setFooter("For more information view !help");
+
+exports.profileTemplate = new Discord.MessageEmbed()
+  .setColor("#FF0000")
+  .setColor("#0099ff")
+  .setTitle("Some title")
+  .setAuthor(
+    "Some name",
+    "https://i.imgur.com/AfFp7pu.png",
+    "https://discord.js.org"
+  )
+  .setDescription("Some description here")
+  .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+  .addFields(
+    { name: "Regular field title", value: "Some value here" },
+    { name: "\u200B", value: "\u200B" },
+    { name: "Inline field title", value: "Some value here", inline: true },
+    { name: "Inline field title", value: "Some value here", inline: true }
+  )
+  .addField("Inline field title", "Some value here", true)
+  .setImage("https://i.imgur.com/AfFp7pu.png")
+  .setTimestamp()
+  .setFooter({
+    text: "Some footer text here",
+    iconURL: "https://i.imgur.com/AfFp7pu.png",
+  });
