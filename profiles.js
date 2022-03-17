634af0a7710bd737f4beca4d01a6a9630fe80c5b
Jordan <j.betts11599@gmail.com>
2022-03-17 17:58:09 +0000

    Added Directory To Data

--- profiles.js
+++ profiles.js
@@ -1,25 +1,24 @@
 const embed = require("./embed.js");
 const sqlite3 = require("sqlite3").verbose();
 
-function getProfile(path, targetChannel, steam64ID) {
+function getProfile(path, targetChannel, Steam64Id) {
   db = new sqlite3.Database(path, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
       return console.error(err.message);
     }
-    console.log(steam64ID);
-    const sqlRead = `SELECT * FROM players WHERE steam64ID = '${steam64ID}'`;
+
+    const sqlRead = `SELECT * FROM players WHERE steam64ID = '${Steam64Id}'`;
 
     db.all(sqlRead, [], (err, rows) => {
       if (err) return console.error(err.message);
 
-      rows.forEach((row) => {
-        console.log(row);
-      });
-
-      db.close((err) => {
-        if (err) return console.error(err.message);
-      });
-      console.log("db closed");
+      if (rows[0] != undefined) {
+        // embed.profileTemplate.setTitle(rows[0])
+        targetChannel.send(embed.profileTemplate);
+      } else {
+        embed.errorMessage.setTitle("⛔ Error: Unknown Player ID");
+        targetChannel.send(embed.errorMessage);
+      }
     });
 
     targetChannel.send("Connected to the Item database.");
